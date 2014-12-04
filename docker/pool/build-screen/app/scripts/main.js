d6eb87ba10d663d4f71f6c349b2963c5cebdfed2
Your Name <you@example.com>
2014-12-04 19:44:26 +0900

    Change port to 443

--- docker/pool/build-screen/app/scripts/main.js
+++ docker/pool/build-screen/app/scripts/main.js
@@ -1,7 +1,7 @@
 (function () {
     showCommitId();
 
-    var ws = new WebSocket("ws://" + document.location.hostname + ":8080");
+    var ws = new WebSocket("ws://" + document.location.hostname + ":443");
     ws.onmessage = function(event){
         if (event.data.match(/FINISHED$/)) {
             document.location.reload();
