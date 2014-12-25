f57d27e9905c093b09d68413dc5e43377328f363
Naoki AINOYA <ainonic@gmail.com>
2014-12-26 01:15:17 +0900

    Stop raising defunct process #53
    - Add API to access docker like docker/pool/builder/lib/builder/git_handler.rb
    - Add API server for docker to EM
    - Change hook.rb to get values via these API then remove "`"
    
    fixed the points below;
    
    **docker/pool/Dockerfile**
    
    - Replaced `FROM` image to [prevsio/poolbase](https://github.com/prevs-io/docker-pool-base).
    - Moved depended package installation process to `docker-pool-base`
    - `curl` with `-s` option to suppress output
    
    **docker/pool/builder/bin/build_server**
    
    - Add `DockerHandler` http api server to eventmachine
    
    **docker/pool/builder/lib/builder/config.rb**
    
    - Add `read_ids` method for reading commit-id/image-id pair from `/app/images/ids`. this method is moved from `hook.rb`
    
    **docker/pool/builder/lib/builder/constants.rb**
    
    - Add the constant of `/app/images/ids`
    
    **docker/pool/builder/lib/builder/docker.rb**
    
    - Utility module for accessing preview containers in pool environment
    - `find_container_by_commit_id` method takes git commit id, returns address (with port number) of matched container
    - the port number of container to preview is fixed; `CONTAINER_PORT=80`
    
    **docker/pool/builder/lib/builder/docker_handler.rb**
    
    - DockerHandler is http server to communicate the container infomation with `hook.rb` in mod_mruby
    - http server has the endpoint `/containers/<git-commit-id>`, and for example, `hook.rb` calls `http://0.0.0.0:9002/containers/a12314zbvd` by `GET` request, then the server returns response `{:status => 'success', :addr => 'xxx.xxx.xxx.xxx:80'}`with json format. If the container corresponding to git commit id doesn't exists, the server returns `{:status => 'error'}`
    
    **docker/pool/handlers/hook.rb**
    
    - Shrinked the codes.
    - moved procedure resolving container address by git commit id to `DockerHandler` server.
    - Stop to executing external command with "`" syntax.
    - Instead using `curl` command to communicate with http server, use mruby module [mruby-webapi](https://github.com/iij/mruby-webapi)
    
    **scripts/init_host_server**
    
    - replace cache file

---
+++ docker/pool/builder/lib/builder/docker.rb
@@ -0,0 +1,38 @@
+require 'docker'
+require 'builder/config'
+
+module Builder
+  module Docker
+    CONTAINER_PORT = 80
+
+    module_function
+    #
+    # Get container address which corresponds to Git commit id.
+    # Returns Docker container id.
+    #
+    # [commit_id]
+    #   Git commit id
+    #
+    def find_container_by_commit_id(commit_id, opts={})
+      logger = opts[:logger] || Logger.new(STDOUT)
+
+      logger.info("Getting container id for commit id<#{commit_id}>")
+      
+      containers = ::Docker::Container.all.map{|c| c.json}
+
+      logger.info("Now number of running containers: #{containers.size}")
+
+      ids = Config.read_ids
+      logger.info("No ids in id file")
+      return nil if ids.empty?
+
+      matched_images = ids.select{|i| i[0] == commit_id}.map{|i| i[1]}
+      return nil if matched_images.empty?
+
+      matched_container =  containers.select{|c| matched_images.include?(c["Image"])}.first
+      return nil unless matched_container
+
+      return "#{matched_container["NetworkSettings"]["IPAddress"]}:#{CONTAINER_PORT}"
+    end
+  end
+end
---
+++ docker/pool/builder/lib/builder/docker_handler.rb
@@ -0,0 +1,41 @@
+require 'eventmachine'
+require 'evma_httpserver'
+require 'builder/constants'
+require 'builder/docker'
+require 'json'
+
+module Builder
+  class DockerHandler < EventMachine::Connection
+    include EventMachine::HttpServer
+    
+    def initialize(*args)
+      super
+      @logger ||= Logger.new(STDOUT)
+      @logger.info("DockerHandler logger is initialized.")
+    end
+  
+    def process_http_request
+      res = EventMachine::DelegatedHttpResponse.new(self)
+
+      if @http_path_info =~ /^\/containers\/(.*)$/
+        commit_id = $1
+        begin
+          res.status = 200
+          addr = Docker.find_container_by_commit_id(commit_id)
+          if addr
+            res.content = JSON.generate({:status => 'success',
+                                        :addr => addr})
+            @logger.info "found container_id: #{res.content}"
+            return res.send_response
+          end
+        rescue => e
+          @logger.info e
+        end
+      end
+
+      res.status = 500
+      res.content = JSON.generate({:status => 'error'})
+      return res.send_response
+    end
+  end
+end
